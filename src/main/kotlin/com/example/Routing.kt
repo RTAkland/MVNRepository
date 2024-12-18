aebdd66fa677af0d55b486b3630077b55402ad84
RTAkland <me@rtast.cn>
2024-12-18 17:52:38 +0800

    initial commit

---
+++ src/main/kotlin/com/example/Routing.kt
@@ -0,0 +1,33 @@
+package com.example
+
+import io.ktor.http.*
+import io.ktor.resources.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.resources.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
+
+fun Application.configureRouting() {
+    install(AutoHeadResponse)
+    install(Resources)
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get<Articles> { article ->
+            // Get all articles ...
+            call.respond("List of articles sorted starting from ${article.sort}")
+        }
+    }
+}
+
+@Serializable
+@Resource("/articles")
+class Articles(val sort: String? = "new")
