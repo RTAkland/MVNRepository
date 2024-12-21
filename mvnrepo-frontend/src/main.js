a411fe33823c7ae8b63823c216e7c91b18cbe9e5
RTAkland <me@rtast.cn>
2024-12-21 16:06:54 +0800

    update frontend

--- mvnrepo-frontend/src/main.js
+++ mvnrepo-frontend/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+let host = window.location.host
+let protocol = window.location.protocol
 
-import { createApp } from 'vue'
-import App from './App.vue'
+document.getElementById("maven-repo-releases")
+    .textContent = `maven("${protocol}://${host}/releases")`
 
-createApp(App).mount('#app')
+document.getElementById("maven-repo-snapshots")
+    .textContent = `maven("${protocol}://${host}/snapshots")`
+
+document.getElementById("maven-repo-private")
+    .textContent = `maven("${protocol}://${host}/private")`
